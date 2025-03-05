a08474c5e7f76a35988facc821cb3aa960a0da3e
beatrizSM3 <beatriz.workspace@gmail.com>
2025-03-05 16:15:23 -0300

    feat: implementação do rag nas siglas

--- tests/tools/utils/preprocess_text.py
+++ tests/tools/utils/preprocess_text.py
@@ -1,19 +1,38 @@
 
 import os
 import json
+import difflib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Obtém o diretório do script atual
 path = os.path.join(BASE_DIR, "..", "utils", "siglas.json")  # Caminho relativo
 
-def remove_siglas(texto: str):
-    """
-    Remove siglas de uma palavra ou frase
-    """
-    with open(path, 'r', encoding='utf-8') as file:
-        dados = json.load(file)
+def get_most_similar_acronym(sigla: str, context: str):
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            dados = json.load(file)
+    except Exception as e:
+        print(f"Erro ao ler o arquivo: {e}")
+        return ""
     
-    substituicoes = {**dados["disciplina"], **dados["curso"]}
-    palavras = texto.split()
-    palavras_modificadas = [substituicoes.get(palavra.upper(), palavra) for palavra in palavras]
-    
-    return " ".join(palavras_modificadas)
+    if not dados:
+        print("JSON não apresenta dados")
+        return ""
+
+    if context not in dados:
+        print(f"Contexto '{context}' não encontrado no JSON.")
+        return ""
+
+    sigla = sigla.upper().strip()
+    lista_siglas = list(dados[context].keys())
+
+    most_similar = difflib.get_close_matches(sigla, lista_siglas, n=1, cutoff=0.3)
+
+    return dados[context][most_similar[0]] if most_similar[0] else ""
+
+#print(get_most_similar_acronym("fm cc 2", "disciplina"))
+
+
+
+
+
+
