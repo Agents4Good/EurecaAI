4b2366c1c137cd8bd4093730bad05c0de91308ff
beatrizSM3 <beatriz.workspace@gmail.com>
2025-03-05 16:20:21 -0300

    fix: adicionando documentação à função get_most_similar_acronym

--- tests/tools/utils/preprocess_text.py
+++ tests/tools/utils/preprocess_text.py
@@ -7,6 +7,16 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Obtém o diretório do 
 path = os.path.join(BASE_DIR, "..", "utils", "siglas.json")  # Caminho relativo
 
 def get_most_similar_acronym(sigla: str, context: str):
+    """
+        Função que utiliza difflib para encontrar a sigla mais similar à sigla informada
+
+        Args:
+            sigla: sigla desejada.
+            context: Contexto da sigla, pode ser disciplina, curso etc. Por exemplo se o contexto informado for 'disciplina' então as siglas a serem consideradas serão as siglas das disciplinas que estão no arquivo json siglas.
+        
+        Returns:
+            String vazia (caso ocorra algum problema) ou nome por extenso da sigla mais próxima à desejada. Ex: se for 'fm cc 2', a função retornará 'fundamentos de matemática para ciência da computação 2'.
+    """
     try:
         with open(path, 'r', encoding='utf-8') as file:
             dados = json.load(file)
