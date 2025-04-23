4cd968e85532b4302b1f0499641dd0e46fcda700
LeviJunior21 <levi.pereira.junior@ccc.ufcg.eu.br>
2025-04-23 09:10:53 -0300

    refact: refatorando o código de estudantes de cursos

--- tests/sql/generate_sql.py
+++ tests/sql/generate_sql.py
@@ -29,5 +29,4 @@ class LLMGenerateSQL:
 
         structured_llm = self.llm.with_structured_output(QueryOutput)
         result = structured_llm.invoke(prompt)
-        print("RESULTADO SQL DENTRO DA CLASSE ", result)
         return {"query": result["query"]}
