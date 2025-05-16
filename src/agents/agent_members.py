632af7273226b8b6f5a0af89ff597dd51a61065e
Matheus Hensley <matheushensley_25@hotmail.com>
2025-05-16 16:04:28 -0300

    Delete src directory

--- src/agents/agent_members.py
+++
@@ -1,9 +0,0 @@
-MEMBERS = [
-    "Agente_Cursos_Eureca", 
-    "Agente_Disciplinas_Turmas_Eureca", 
-    "Agente_Campus_Eureca", 
-    "Agente_Setor_Professor_Estagio_Eureca",
-    "Agente_Detector",
-]
-
-OPTIONS = ("FINISH",) + tuple(MEMBERS)
