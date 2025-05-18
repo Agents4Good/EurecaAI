085ba7d78a8f9a02a42acf4e010252453d7c7d41
mathensley <matheushensley_25@hotmail.com>
2025-05-18 17:02:54 -0300

    fix: adicionando static path no flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/eureca-chat/static")
 
 # Inicializa o sistema de agentes ao iniciar a aplicação
 system = EurecaChat(
