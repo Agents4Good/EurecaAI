b4b01c8b0a81b77869a6dbcc04f4f500fa6a493a
mathensley <matheushensley_25@hotmail.com>
2025-05-19 12:12:36 -0300

    refact: adicionando dotenv no modelo de RAG

--- flask_app/langchain_models.py
+++ flask_app/langchain_models.py
@@ -1,4 +1,7 @@
 from langchain_community.chat_models import ChatDeepInfra
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 # TESTE: para ser utilizado nas tools que fazem RAG
 model = ChatDeepInfra(model="meta-llama/Llama-3.3-70B-Instruct", temperature=0)
