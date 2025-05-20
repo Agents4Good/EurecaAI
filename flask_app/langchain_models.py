1ae701160c2cf4bf0c039cbd8e9c3bdf17d264ac
mathensley <matheushensley_25@hotmail.com>
2025-05-20 14:15:31 -0300

    startando branch para lidar com memória no langchain

--- flask_app/langchain_models.py
+++ flask_app/langchain_models.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-# TESTE: para ser utilizado nas tools que fazem RAG
+# Para ser utilizado nas tools que fazem RAG
 model = ChatDeepInfra(model="meta-llama/Llama-3.3-70B-Instruct", temperature=0)
